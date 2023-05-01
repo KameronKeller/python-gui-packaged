e9ed4cfcb4a3e19f6e71a48c767ea96a0eaeae06
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-01 15:38:13 -0700

    Add main function

--- name_converter.py
+++ name_converter.py
@@ -19,25 +19,21 @@ def write_anonymized_row(csv_writer, row):
 	# Write the row
 	csv_writer.writerow({'id': id, 'first_name': first_name, 'last_name': last_name, 'phone': phone})
 
+def main():
+	with open(csv_input_file) as csv_input:
+		csv_reader = csv.DictReader(csv_input)
 
+		# Grab the header
+		header = csv_reader.fieldnames
 
-with open(csv_input_file) as csv_input:
-	csv_reader = csv.DictReader(csv_input)
+		with open(csv_output_file, 'w') as csv_output:
+			csv_writer = csv.DictWriter(csv_output, fieldnames=header)
 
-	# Grab the header
-	header = csv_reader.fieldnames
+			# Write the header to a new file
+			csv_writer.writeheader()
 
-	with open(csv_output_file, 'w') as csv_output:
-		csv_writer = csv.DictWriter(csv_output, fieldnames=header)
+			# For each row in the original file, anonymize the data and write to a new file
+			for i, row in enumerate(csv_reader):
+				write_anonymized_row(csv_writer, row)
 
-		# Write the header to a new file
-		csv_writer.writeheader()
-
-		# For each row in the original file, anonymize the data and write to a new file
-		for i, row in enumerate(csv_reader):
-			write_anonymized_row(csv_writer, row)
-
-
-
-
-
+main()
