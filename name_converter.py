5a1ba4e7450ebaea73852d98f6a5cfe9c65a3e28
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-02 06:53:00 -0700

    Create anonymizer class and refactor

--- name_converter.py
+++ name_converter.py
@@ -1,41 +1,47 @@
 import csv
 import sys
 
-def anonymize_name(name):
-	return name[0] + '.'
+class DataAnonymizer:
+	def __init__(self, csv_input_file, csv_output_file):
+		self.csv_input_file = csv_input_file
+		self.csv_output_file = csv_output_file
 
-def write_anonymized_row(csv_writer, row):
-	# Get the data from the row
-	id = row['id']
-	first_name = row['first_name']
-	last_name = row['last_name']
-	phone = row['phone']
+	def anonymize_name(self, name):
+		return name[0] + '.'
 
-	last_name = anonymize_name(last_name)
+	def write_anonymized_row(self, csv_writer, row):
+		# Get the data from the row
+		id = row['id']
+		first_name = row['first_name']
+		last_name = row['last_name']
+		phone = row['phone']
 
-	# Write the row
-	csv_writer.writerow({'id': id, 'first_name': first_name, 'last_name': last_name, 'phone': phone})
+		last_name = self.anonymize_name(last_name)
 
-def anonymize_data(csv_input_file, csv_output_file):
-	with open(csv_input_file) as csv_input:
-		csv_reader = csv.DictReader(csv_input)
+		# Write the row
+		csv_writer.writerow({'id': id, 'first_name': first_name, 'last_name': last_name, 'phone': phone})
 
-		# Grab the header
-		header = csv_reader.fieldnames
+	def anonymize_data(self):
+		with open(self.csv_input_file) as csv_input:
+			csv_reader = csv.DictReader(csv_input)
 
-		with open(csv_output_file, 'w') as csv_output:
-			csv_writer = csv.DictWriter(csv_output, fieldnames=header)
+			# Grab the header
+			header = csv_reader.fieldnames
 
-			# Write the header to a new file
-			csv_writer.writeheader()
+			with open(self.csv_output_file, 'w') as csv_output:
+				csv_writer = csv.DictWriter(csv_output, fieldnames=header)
 
-			# For each row in the original file, anonymize the data and write to a new file
-			for i, row in enumerate(csv_reader):
-				write_anonymized_row(csv_writer, row)
+				# Write the header to a new file
+				csv_writer.writeheader()
+
+				# For each row in the original file, anonymize the data and write to a new file
+				for i, row in enumerate(csv_reader):
+					self.write_anonymized_row(csv_writer, row)
 
 def main():
 	csv_input_file = sys.argv[1]
 	csv_output_file = sys.argv[2]
-	anonymize_data(csv_input_file, csv_output_file)
+	data_anonymizer = DataAnonymizer(csv_input_file, csv_output_file)
+	data_anonymizer.anonymize_data()
 
 main()
