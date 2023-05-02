ae2b217c98ed16c0960f0a52b74168ff73dd86a1
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-02 08:56:00 -0700

    Re-order logic

--- simple_gui.py
+++ simple_gui.py
@@ -2,9 +2,9 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Basic Example")
+root.minsize(500,250)
 
 button = tk.Button(text="This is a button")
 button.pack()
 
-root.minsize(500,250)
 root.mainloop()
